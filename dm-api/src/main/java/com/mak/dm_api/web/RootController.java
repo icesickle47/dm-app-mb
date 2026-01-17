9d514c181e9194b28d2195dbdedd3f61dc417526
God <makmayank047@gmail.com>
2026-01-17 14:38:41 +0530

    added RootController - this makes sure we have a live page on localhost8080 once we are done running and setting up dm-api - our API IS LIVE!

--- dm-api/src/main/java/com/mak/dm_api/web/RootController.java
+++ dm-api/src/main/java/com/mak/dm_api/web/RootController.java
@@ -1,2 +1,13 @@
-package com.mak.dm_api.web;public class RootController {
+package com.mak.dm_api.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "dm-api is running ✅";
+    }
 }
